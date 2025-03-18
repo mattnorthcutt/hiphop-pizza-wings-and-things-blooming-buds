40ef549029503212c5ab21f6e634e06587318844
CalebTds <calebchittom@gmail.com>
2025-03-17 20:07:30 -0500

    scss1

--- components/loginButton.js
+++ components/loginButton.js
@@ -2,7 +2,7 @@ import { signIn } from '../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button href="login" id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = '<button href="login" id="google-auth" class="btn glogin">GOOGLE LOGIN</button>';
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
