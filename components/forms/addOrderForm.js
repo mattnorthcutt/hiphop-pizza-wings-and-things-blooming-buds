58efb122fd88ef7e3ad642f46c67db703ff133e1
CalebTds <calebchittom@gmail.com>
2025-03-18 20:19:17 -0500

    2ndscss

--- components/forms/addOrderForm.js
+++ components/forms/addOrderForm.js
@@ -7,25 +7,25 @@ const addOrderForm = (obj = {}) => {
     <form id="${obj.firebaseKey ? `update-order--${obj.firebaseKey}` : 'submit-order'}" class="mb-4">
       <div class="form-group">
         <label for="orderType"></label>
-        <select id="orderType" style="width: 600px; text-align: center; border-radius: 5px; margin-bottom: 10px;" class="mt-3" required>
+        <select id="orderType" style="width: 350px; text-align: center; border-radius: 5px; margin-bottom: 10px;" class="mt-3" required>
           <option value="select">Select an Order Type</option>
           <option value="Phone">Phone</option>
           <option value="inPerson">In-Person</option>
         </select>
       </div>
-      <div class="form-group">
-        <label style="color: black; margin-bottom: 5px;" for="title">Order Name</label>
-        <input type="text" style=" " class="form-control" id="name" aria-describedby="orderName" placeholder="Enter Name" value="${obj.name || ''}" required>
+      <div class="form-group" id="Cao-form">
+        <label style="margin-bottom: 5px;" for="title">Order Name</label>
+        <input type="text" class="form-control" id="name" aria-describedby="orderName" placeholder="Enter Name" value="${obj.name || ''}" required>
       </div>
-      <div class="form-group">
-        <label style="color: black; margin-top: 10px; margin-bottom: 5px;" for="phone">Customer Phone Number</label>
+      <div class="form-group" id="Cao-form">
+        <label style="margin-top: 10px; margin-bottom: 5px;" for="phone">Customer Phone Number</label>
         <textarea class="form-control" style="height: 150px;" placeholder="Enter Phone Number" id="phone" >${obj.phoneNum || ''}</textarea>
       </div>
-      <div class="form-group">
-        <label style="color: black; margin-top: 10px; margin-bottom: 5px;" for="phone">Customer Email</label>
+      <div class="form-group" id="Cao-form">
+        <label style="margin-top: 10px; margin-bottom: 5px;" for="phone">Customer Email</label>
         <textarea class="form-control" style="height: 150px;" placeholder="Enter Email" id="email" >${obj.email || ''}</textarea>
       </div>
-      <button class="mt-2" style="color: black; border-radius: 5px;"type="submit" class="btn btn-primary">Create Order
+      <button class="mt-2" style="width: 350px; color: black; border-radius: 5px;"type="submit" class="btn btn-primary">Create Order
       </button>
     </form>`;
 
--- pages/home.js
+++ pages/home.js
@@ -5,11 +5,11 @@ const home = () => {
   clearDom();
 
   const domString = `<div style="display: flex; flex-direction: column; align-items: center;"id="homePage">
-    <h3 style="color: white; margin-bottom: 60px;" class="home-title">Welcome!</h3>
-    <button class="btn btn-white" style="background-color: white; color: black; margin-bottom: 60px; width: 220px" type="button" id="home-menu">View Menu</button>
-    <button class="btn btn-white" style="background-color: white; color: black; margin-bottom: 60px; width: 220px" type="button" id="home-view">View Orders</button>
-    <button class="btn btn-white" style="background-color: white; color: black; margin-bottom: 60px; width: 220px" type="button" id="home-create">Create an Order</button>
-    <button class="btn btn-white" style="background-color: white; color: black; margin-bottom: 60px; width: 220px" type="button" id="home-revenue">View Revenue</button>
+    <h3 style="color: white; margin-bottom: 60px;" class="home-title">Welcome to Hip Hop's!</h3>
+    <button class="btn VM" style="color: black; margin-bottom: 45px; width: 220px" type="button" id="home-menu">View Menu</button>
+    <button class="btn VO" style="color: black; margin-bottom: 45px; width: 220px" type="button" id="home-view">View Orders</button>
+    <button class="btn CaO" style="color: black; margin-bottom: 45px; width: 220px" type="button" id="home-create">Create an Order</button>
+    <button class="btn VR" style="color: black; margin-bottom: 45px; width: 220px" type="button" id="home-revenue">View Revenue</button>
     </div>
  `;
 
